b53e9da499650e471735f070c0d7cb68fb265b0c
ankur <ankurbansal153@gmail.com>
2024-08-17 13:09:34 +0530

    Complete Project

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,22 +1,23 @@
 class ApiError extends Error {
     constructor(
         statusCode,
-        message = "Something went wrong",
-        error = [],
-        statck = ""
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
     ){
-        super(message);
-        this.statusCode = statusCode;
-        this.data = null;
-        this.message = message;
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
         this.success = false;
-        this.errors = errors;
+        this.errors = errors
 
-        if(statck){
-            this.stack = statck
-        }else{
+        if (stack) {
+            this.stack = stack
+        } else{
             Error.captureStackTrace(this, this.constructor)
         }
+
     }
 }
 
